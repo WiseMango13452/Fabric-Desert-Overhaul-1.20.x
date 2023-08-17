1662f97ff091fcce15d681a94997d2e471fbc08c
WiseMango13452 <rabbitcreator2@gmail.com>
2023-08-17 02:03:04 +0200

    Initial commit

---
+++ src/main/java/net/mango/desertoverhaul/DesertOverhaulClient.java
@@ -0,0 +1,10 @@
+package net.mango.desertoverhaul;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DesertOverhaulClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mango/desertoverhaul/DesertOverhaulDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mango.desertoverhaul;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DesertOverhaulDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
