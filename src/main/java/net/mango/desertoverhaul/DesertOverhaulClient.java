8a4b8de77262ac8b7518bd221b280124ac6a5525
WiseMango13452 <rabbitcreator2@gmail.com>
2023-08-18 18:05:28 +0200

    palm log + leaves alpha

--- src/main/java/net/mango/desertoverhaul/DesertOverhaulClient.java
+++ src/main/java/net/mango/desertoverhaul/DesertOverhaulClient.java
@@ -1,10 +1,23 @@
 package net.mango.desertoverhaul;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.mango.desertoverhaul.block.ModBlocks;
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.color.world.FoliageColors;
+import net.minecraft.world.BlockRenderView;
 
 public class DesertOverhaulClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
-
+        BlockColors blockColors = new BlockColors();
+        /*ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> {
+            if (world == null || pos == null) {
+                return FoliageColors.getDefaultColor();
+            }
+            return BiomeColors.getFoliageColor(world, pos);
+        }. DesertOverhaul.PALM_LEAVES);*/
     }
 }
--- src/main/java/net/mango/desertoverhaul/block/ModBlocks.java
+++ src/main/java/net/mango/desertoverhaul/block/ModBlocks.java
@@ -5,14 +5,19 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.mango.desertoverhaul.DesertOverhaul;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.PillarBlock;
+import net.minecraft.block.*;
+import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.color.world.FoliageColors;
+import net.minecraft.client.color.world.GrassColors;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
@@ -20,15 +25,16 @@ public class ModBlocks {
     public static final Block PALM_LOG = registerBlock("palm_log",
             new PillarBlock(FabricBlockSettings.copyOf(Blocks.JUNGLE_LOG)));
 
-    public static final Block PALM_LEAVES = registerBlock("palm_leaves",
-            new Block(FabricBlockSettings.copyOf(Blocks.JUNGLE_LEAVES)));
+    public static final Block PALM_LEAVES = Blocks.register("palm_leaves",
+            new LeavesBlock(FabricBlockSettings.copyOf(Blocks.JUNGLE_LEAVES)));
 
     private static void addBlockToBuildingBlockGroup(FabricItemGroupEntries entries) {
         entries.add(PALM_LOG);
     }
+
     private static void addBlockToNaturalsGroup(FabricItemGroupEntries entries) {
-        entries.add(PALM_LOG);
-        entries.add(PALM_LEAVES);
+        entries.add(new ItemStack(PALM_LOG));
+        entries.add(new ItemStack(PALM_LEAVES));
     }
 
     private static Block registerBlock(String name, Block block) {
